43444b33d1b82bcc13a185fc48cb80b7334275b2
Noy Dvori <144912383+noydvori@users.noreply.github.com>
2024-01-22 11:36:12 +0200

    Delete lists.ml

--- lists.ml
+++
@@ -1,7 +0,0 @@
-let rec sum_list l = match l with
-| [] -> 0
-| x :: u -> x + sum_list u;;
-
-let rec compress l = match l with
-| one :: (two :: tail) -> if (one = two) then compress(two::tail) else one::(compress (two::tail))
-| smaller -> smaller;;
